fcb8ae4e774c6706d026470b78bcd9e2c2ea3882
alifdanielhensem-creator <alifdanielhensem@gmail.com>
2025-12-04 21:39:44 +0800

    Add tempahSekarang function for WhatsApp booking

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+function tempahSekarang() {
+    alert("Terima kasih! Sila WhatsApp kami untuk lokasi.");
+    // Di sini nanti kita boleh letak link WhatsApp sebenar
+    window.location.href = "https://wa.me/60123456789?text=Nak%20book%20Prebet"; 
+}
